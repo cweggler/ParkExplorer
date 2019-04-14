48a8da321db45d9312d87adda329496ddb67034c
Cara Weggler <cweggler@gmail.com>
2019-04-14 11:34:39 -0500

    refine model classes to have an image associated with each photo data piece

--- NationalParkExplorer/Model/FlickrImage.swift
+++ NationalParkExplorer/Model/FlickrImage.swift
@@ -19,7 +19,12 @@ class FlickrImage {
     }
     
     var thumbnailURL: String? {
-        guard let photo = photoData else {return nil }
-        return "https://farm\(photo.farm)"
+        guard let photo = photoData else { return nil }
+        return "https://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_q.jpg"
+    }
+    
+    var fullURL: String? {
+        guard let photo = photoData else { return nil }
+        return "https://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_h.jpg"
     }
 }
