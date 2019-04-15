05b9de29604a8410e32dd62dc49eb1b9d31dd2ae
Cara Weggler <cweggler@gmail.com>
2019-04-14 21:30:20 -0500

    added a placeholder image and added code to load that while waiting for the images to load

--- NationalParkExplorer/Controller/ImageCollectionViewController.swift
+++ NationalParkExplorer/Controller/ImageCollectionViewController.swift
@@ -12,7 +12,7 @@ class ImageCollectionViewController: UICollectionViewController {
     
     var park: NationalPark?
     let reuseIdentifier = "UICollectionViewCell"
-    
+    var photoSet: FlickrPhotoSet? // add a global FlickrPhotoSet to help with retrieving that data
     let flickrservice = FlickrService()
     
     override func viewDidLoad() {
@@ -31,9 +31,49 @@ class ImageCollectionViewController: UICollectionViewController {
                     }
                     else {
                         print(photos)
-                        // TODO download thumbnails for photos and display in collection view
+                        self.photoSet = FlickrPhotoSet(photos: photos) //initialize
+                        self.collectionView.reloadData() //tells collectionview to reload to display the photos
                     }
                 }
+            }
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoSet?.count ?? 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCell
+        
+        // if already thumbnail, set and return
+        // else if no thumbnail, set placeholder, initiate request
+        // the completion handler will refresh the cell once image is downloaded
+        
+        guard let photoSet = photoSet else {
+            cell.image.image = UIImage(named: "placeholder")
+            return cell
+        }
+        
+        if let thumbnail = photoSet.images[indexPath.row].thumbnail {
+            cell.image.image = thumbnail
+        }
+        
+        else {
+            cell.image.image = UIImage(named: "placeholder")
+            requestThumbnail(for: indexPath.row)
+        }
+        
+        return cell
+    }
+    
+    func requestThumbnail(for index: Int) {
+        let imageData = self.photoSet?.images[index]
+        
+        flickrservice.downloadImage(url: imageData!.thumbnailURL!) { (thumbnail: UIImage?, error: Error?) in self.photoSet?.images[index].thumbnail = thumbnail
+            DispatchQueue.main.async {
+                self.collectionView.reloadItems(at: [IndexPath(row: index, section: 0)])
             }
         }
     }
